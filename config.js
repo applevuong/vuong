b4f5d296c651167563296aa9606a66e09ef5eda9
applevuong <106525401+applevuong@users.noreply.github.com>
2022-05-30 18:05:35 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const CONFIG = {
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'Quyen Kuk CHim',
+    btnIntro: '^^HiHi^^',
     title: 'Làm người yêu tớ nha 🥰',
     desc: '',
     btnYes: 'Thích lắm <33',
@@ -16,5 +16,5 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/vuong.app.92'
+    messLink: 'https://www.facebook.com/groups/tek4.vn/'
 }
