795930f662f50ec5bb45093e82c85e0aa1c78be2
Apple <106525401+applevuong@users.noreply.github.com>
2025-06-10 13:57:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,19 +2,19 @@ const CONFIG = {
     titleWeb: "Love confess",
     introTitle: 'Who are you',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao Xuân Xuân cứ đi nhầm đường
+    Lạc vào tim Vương Vương lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Làm người yêu tớ nha 🥰',
+    title: 'Làm người vợ vương vương nha Xuân Xuân 🥰',
     desc: '',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Cho tớ biết lí do đi',
+    question: 'Cho ox biết lí do đi',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Tối nay 7h,OX vương qua đón bx Xuân đi đấm lưng nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/groups/tek4.vn/'
+    messLink: 'https://www.facebook.com/vuong.tran.495286'
 }
